bb22d003482ff751de7fb9c5b441218169b4ffb7
Sara Gibbons <saragibby@gmail.com>
2019-06-15 14:18:44 -0400

    drop in styles from current site
    I mean they don't work, but at least they are here so figure out how to bake in

--- www/components/Map.js
+++ www/components/Map.js
@@ -4,6 +4,17 @@ import styled from 'styled-components';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+ const customStyles = [
+   {
+     "featureType":"administrative",
+     "elementType":"geometry.fill",
+     "stylers":[
+       {"visibility":"on"},
+       {"color":"#000000"}
+     ]
+   },{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative","elementType":"labels.icon","stylers":[{"hue":"#ff0000"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.man_made","elementType":"labels","stylers":[{"saturation":"36"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ff9100"},{"visibility":"on"}]}]
+
+
 class Map extends Component {
   static defaultProps = {
     center: {
@@ -20,6 +31,9 @@ class Map extends Component {
           bootstrapURLKeys={{ key: "AIzaSyAyDvd2WtMiZPOlruEguE6NNRESVqDeXao" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          defaultOptions={{
+            styles: customStyles
+          }}
         >
           <AnyReactComponent
             lat={this.props.center.lat}
