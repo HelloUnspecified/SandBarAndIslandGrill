ab6eef7245029e6ea1c7f4c8656866f56a7befc8
Sara Gibbons <saragibby@gmail.com>
2019-06-22 16:03:55 -0400

    add map styling

--- www/components/Map.js
+++ www/components/Map.js
@@ -5,14 +5,94 @@ import styled from 'styled-components';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
  const customStyles = [
-   {
-     "featureType":"administrative",
-     "elementType":"geometry.fill",
-     "stylers":[
-       {"visibility":"on"},
-       {"color":"#000000"}
-     ]
-   },{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative","elementType":"labels.icon","stylers":[{"hue":"#ff0000"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.man_made","elementType":"labels","stylers":[{"saturation":"36"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ff9100"},{"visibility":"on"}]}]
+  {
+    featureType: "administrative",
+    elementType: "geometry.fill",
+    stylers:[
+      { visibility: "on" },
+      { color: "#000000" }
+    ]
+  },
+  {
+    featureType: "administrative",
+    elementType: "labels.text.fill",
+    stylers: [
+      { color: "#444444" }
+    ]
+  },
+  {
+    featureType: "administrative",
+    elementType: "labels.icon",
+    stylers: [
+      { hue: "#ff0000" }
+    ]
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "geometry.stroke",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "all",
+    stylers: [
+      { color: "#f2f2f2" }
+    ]
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "labels",
+    stylers: [
+      { saturation: "36" }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+      { saturation: -100 },
+      { lightness: 45 }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "all",
+    stylers: [
+      { visibility: "simplified" }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.icon",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      { color: "#ff9100" },
+      { visibility: "on" }
+    ]
+  }
+]
 
 
 class Map extends Component {
@@ -28,10 +108,10 @@ class Map extends Component {
     return (
       <div style={{ height: '300px', width: '100%', top: '10px', position: 'relative' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAyDvd2WtMiZPOlruEguE6NNRESVqDeXao" }}
+          bootstrapURLKeys={{ key: "AIzaSyCYvXhXHHpr4_n1gRVxWv2Au0IFDZqc2RU" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-          defaultOptions={{
+          options={{
             styles: customStyles
           }}
         >
