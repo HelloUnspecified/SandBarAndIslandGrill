346e549bcd9b8106516567b7c611b0f2be7d58c0
Sara Gibbons <saragibby@gmail.com>
2019-09-02 17:28:47 -0400

    add apollo client

--- www/pages/_app.js
+++ www/pages/_app.js
@@ -2,6 +2,16 @@ import React from 'react';
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import fetch from 'node-fetch'
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graph",
+  fetch: fetch
+});
+
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
@@ -17,11 +27,13 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
-        <Page>
-          <Component {...pageProps} />
-        </Page>
-      </Container>
+      <ApolloProvider client={client}>
+        <Container>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </Container>
+      </ApolloProvider >
     );
   }
 }
--- www/pages/menu.js
+++ www/pages/menu.js
@@ -1,19 +1,42 @@
 import styled from 'styled-components';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { Fragment } from 'react';
-import Hero from '../components/Hero'
+import Hero from '../components/Hero';
 
-const menu = props => (
-  <ParallaxProvider>
-    <Fragment>
-      <Hero
-        imagePath="/static/images/tacos-and-rings.jpg"
-        heading="Sandbar and Island Grill"
-        subheading="Menu"
-      />
-      <p>Menu</p>
-    </Fragment>
-  </ParallaxProvider>
-);
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+
+const menu = props => {
+
+  const { loading, error, data } = useQuery(gql`
+    {
+      dinners {
+        id
+        name
+      }
+    }
+  `);
+
+  console.log(error);
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+  
+  return (
+    <ParallaxProvider>
+      <Fragment>
+        <Hero
+          imagePath="/static/images/tacos-and-rings.jpg"
+          heading="Sandbar and Island Grill"
+          subheading="Menu"
+        />
+        <p>Menu</p>
+      </Fragment>
+    </ParallaxProvider>
+  )
+};
 
 export default menu;
